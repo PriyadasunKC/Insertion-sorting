ba293158d34cdf4f19dc2d81cf6da79009bb2703
Priyadasun K.C <117230069+PriyadasunKC@users.noreply.github.com>
2023-02-17 00:25:23 +0530

    Update insertion_sort.c
    Add the code to get the execution time

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
-
+#include <time.h>
+# include <stdlib.h>
 int main()
 {
+    clock_t start, end;
+    start = clock();
     int arr [5] = {2,5,8,6,0};
     int size = 5;
     int key = 0;
     int sortedFirst = 0;
-    
+
     for(int k = 1; k< size;k++){
         key = arr[k];
         sortedFirst = k-1;
-        
+
         while(sortedFirst >= 0 && key < arr[sortedFirst]){
             arr[sortedFirst+1] = arr[sortedFirst];
             sortedFirst = sortedFirst -1;
         }
         arr[sortedFirst+1] = key;
     }
-    
+
     for(int p = 0; p <size;p++){
         printf("%d ",arr[p]);
     }
+    end = clock();
+    double duration = ((double)end - start)/CLOCKS_PER_SEC;
+
+    printf("\nTime taken to execute in seconds : %f", duration);
     return 0;
 }
